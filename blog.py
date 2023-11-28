759bbdc56e2f771d1eae7c6e39941a3e2eb619d0
D0Z0R0V <denielmorozov8000@gmail.com>
2023-11-28 22:51:36 +0300

    config and form flask

--- blog.py
+++ blog.py
@@ -1,26 +1,22 @@
 from flask import Flask, render_template
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 
 @app.route("/")
 def index():
     user = {"username": "Дениель"}
     posts = [
-        {
-            "author": {"username": "John"}, 
-            "body": "Beautiful day in Portland!"
-            },
-        {
-            "author": {"username": "Susan"}, 
-            "body": "The Avengers movie was so cool!"
-            },
+        {"author": {"username": "John"}, "body": "Beautiful day in Portland!"},
+        {"author": {"username": "Susan"}, "body": "The Avengers movie was so cool!"},
         {
             "author": {"username": "Ипполит"},
             "body": "Какая гадость эта ваша заливная рыба!!",
         },
     ]
-    
+
     return render_template("index.html", users=user, posts=posts)
 
 
