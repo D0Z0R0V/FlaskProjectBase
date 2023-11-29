3b9a9fdd4812df6b8c76866f15c0d04fc2ecec11
D0Z0R0V <denielmorozov8000@gmail.com>
2023-11-29 21:18:55 +0300

    form login

--- blog.py
+++ blog.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for
 from config import Config
+from form import LoginForm
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -9,8 +10,14 @@ app.config.from_object(Config)
 def index():
     user = {"username": "Дениель"}
     posts = [
-        {"author": {"username": "John"}, "body": "Beautiful day in Portland!"},
-        {"author": {"username": "Susan"}, "body": "The Avengers movie was so cool!"},
+        {
+            "author": {"username": "John"}, 
+            "body": "Beautiful day in Portland!"
+        },
+        {
+            "author": {"username": "Susan"}, 
+            "body": "The Avengers movie was so cool!"
+        },
         {
             "author": {"username": "Ипполит"},
             "body": "Какая гадость эта ваша заливная рыба!!",
@@ -19,6 +26,15 @@ def index():
 
     return render_template("index.html", users=user, posts=posts)
 
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', form=form, title='Sign In')
 
 if __name__ == "__main__":
     app.run(debug=True)
